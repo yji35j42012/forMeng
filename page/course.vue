44d3248a95e5dbb7d15c927d7ff79341c46dfca9
yji35j4212 <yji35j4212@gmail.com>
2022-09-22 18:16:42 +0800

    檔案同步

---
+++ page/course.vue
@@ -0,0 +1,19 @@
+<style scoped></style>
+
+<template>
+    <div class="container">course</div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {};
+    },
+    components: {},
+    mounted() {
+        // console.log(store.state.nowPage);
+    },
+    computed: {},
+    methods: {},
+};
+</script>
