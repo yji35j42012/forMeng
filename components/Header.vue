db1c0fe9190c7da42d1994477bd065a1c04751c9
yji35j4212 <yji35j4212@gmail.com>
2022-09-23 14:38:02 +0800

    檔案同步

---
+++ components/Header.vue
@@ -0,0 +1,55 @@
+<style scoped></style>
+
+<template>
+    <div class="header">
+        <div class="logo">
+            <h1>
+                <img class="logo_pc" src="./images/logo_pc.png" alt="" />
+                <img class="logo_ph" src="./images/logo_ph.png" alt="" />
+            </h1>
+        </div>
+        <button class="menu_btn" @click="menuHandler">
+            <span></span>
+        </button>
+        <ul id="menu" class="menu">
+            <li class="menu_item">
+                <router-link to="/">首頁</router-link>
+            </li>
+            <li class="menu_item">
+                <router-link to="/course">廚藝教室</router-link>
+            </li>
+            <li class="menu_item">
+                <router-link to="/">節氣食補</router-link>
+            </li>
+            <li class="menu_item">
+                <router-link to="/">素食地圖</router-link>
+            </li>
+            <li class="menu_item">
+                <router-link to="/">相關文章</router-link>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+module.exports = {
+    data() {
+        return {};
+    },
+    components: {},
+    mounted() {
+        // console.log(store.state.nowPage);
+    },
+    computed: {},
+    methods: {
+        menuHandler() {
+            var menu = document.querySelector("#menu");
+            if (menu.classList.contains("on")) {
+                menu.classList.remove("on");
+            } else {
+                menu.classList.add("on");
+            }
+        },
+    },
+};
+</script>
--- page/course.vue
+++ page/course.vue
@@ -1,7 +1,44 @@
 <style scoped></style>
 
 <template>
-    <div class="container">course</div>
+    <div class="container">
+        <div class="infoHead">
+            <h1>廚藝教室</h1>
+            <div class="infoHead_pic">
+                <img src="../images/course/mascot_title.png" alt="" />
+            </div>
+        </div>
+        <div class="infoBody">
+            <div class="card">
+                <div class="card_pic">
+                    <img src="../images/course/course001-1.jpg" alt="" />
+                </div>
+                <div class="card_txt">
+                    <h2>地瓜餅 (奶素)</h2>
+                    <p class="border">
+                        超級食物「地瓜」含有豐富的膳食纖維、β-胡蘿蔔素、鎂、鉀等營養素。製作成地瓜餅當早餐或點心都很適合...
+                    </p>
+                    <div class="card_price">
+                        課程價格
+                        <span>$1,200</span>
+                    </div>
+                    <ul class="card_time">
+                        <li>開課時間</li>
+                        <li>
+                            10月21日(五) 19:00-21:00 <br />
+                            11月18日(五) 19:00-21:00
+                        </li>
+                    </ul>
+                    <div class="card_fun">
+                        <button class="normal_btn">了解更多</button>
+                    </div>
+                </div>
+            </div>
+            <div class="filter">
+                <h2>條件篩選</h2>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
