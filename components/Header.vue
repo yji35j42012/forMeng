aaf944ed2d827ccea96481aaf7b37d8753af3665
yji35j4212 <yji35j4212@gmail.com>
2022-09-26 11:57:10 +0800

    檔案同步

--- components/Header.vue
+++ components/Header.vue
@@ -1,63 +1,67 @@
 <style scoped></style>
 
 <template>
-    <div class="header">
-        <div class="logo">
-            <h1>
-                <img class="logo_pc" src="./images/logo_pc.png" alt="" />
-                <img class="logo_ph" src="./images/logo_ph.png" alt="" />
-            </h1>
-        </div>
-        <button id="menu_btn" class="menu_btn" @click="menuHandler">
-            <span></span>
-        </button>
-        <ul class="menu">
-            <li class="menu_item">
-                <router-link to="/">首頁</router-link>
-            </li>
-            <li class="menu_item">
-                <router-link to="/course">廚藝教室</router-link>
-            </li>
-            <li class="menu_item">
-                <router-link to="/">節氣食補</router-link>
-            </li>
-            <li class="menu_item">
-                <router-link to="/">素食地圖</router-link>
-            </li>
-            <li class="menu_item">
-                <router-link to="/">相關文章</router-link>
-            </li>
-        </ul>
-    </div>
+	<div class="header">
+		<div class="logo">
+			<h1>
+				<img class="logo_pc" src="./images/logo_pc.png" alt="" />
+				<img class="logo_ph" src="./images/logo_ph.png" alt="" />
+			</h1>
+		</div>
+		<button id="menu_btn" class="menu_btn" @click="menuHandler">
+			<span></span>
+		</button>
+		<ul class="menu">
+			<li class="menu_item">
+				<router-link to="/">首頁</router-link>
+			</li>
+			<li class="menu_item">
+				<router-link to="/course">廚藝教室</router-link>
+			</li>
+			<li class="menu_item">
+				<router-link to="/">節氣食補</router-link>
+			</li>
+			<li class="menu_item">
+				<router-link to="/">素食地圖</router-link>
+			</li>
+			<li class="menu_item">
+				<router-link to="/">相關文章</router-link>
+			</li>
+			<li class="menu_item">
+				<i class="icon_car" v-html="icon_all.car"></i>
+				<i class="icon_member" v-html="icon_all.member"></i>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <script>
 module.exports = {
-    data() {
-        return {};
-    },
-    components: {},
-    mounted() {
-        // console.log(store.state.nowPage);
-    },
-    computed: {},
-    watch: {
-        $route(to, from) {
-            var menu = document.querySelector("#menu");
-            if (menu_btn.classList.contains("on")) {
-                menu.classList.remove("on");
-            }
-        },
-    },
-    methods: {
-        menuHandler() {
-            var menu_btn = document.querySelector("#menu_btn");
-            if (menu_btn.classList.contains("on")) {
-                menu_btn.classList.remove("on");
-            } else {
-                menu_btn.classList.add("on");
-            }
-        },
-    },
+	data() {
+		return {
+			icon_all: icon_all,
+		};
+	},
+	components: {},
+	mounted() {
+		// console.log(store.state.nowPage);
+	},
+	computed: {},
+	watch: {
+		$route(to, from) {
+			var menu_btn = document.querySelector("#menu_btn");
+			menu_btn.classList.remove("on");
+		},
+	},
+	methods: {
+		menuHandler() {
+			var menu_btn = document.querySelector("#menu_btn");
+			if (menu_btn.classList.contains("on")) {
+				menu_btn.classList.remove("on");
+			} else {
+				menu_btn.classList.add("on");
+			}
+		},
+	},
 };
 </script>
--- js/icon_all.js
+++ js/icon_all.js
@@ -1,8 +1,25 @@
 var icon_all = {
     select_arr: `<svg width="17" height="10" viewBox="0 0 17 10" fill="none" xmlns="http:/www.w3.org2000/svg"><path d="M1.5 1.5L8.5 8.5L15.5 1.5" stroke="#005F5F" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round" /> </svg>`,
-    keep:`<svg width="25" height="32" viewBox="0 0 25 32" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 30V3.71875C1.5 2.21721 2.7312 1 4.25 1H20.75C22.2688 1 23.5 2.21721 23.5 3.71875V30L12.5 23.6562L1.5 30Z" stroke="#333333" stroke-width="2"/> </svg>
+    keep: `<svg width="25" height="32" viewBox="0 0 25 32" xmlns="http://www.w3.org/2000/svg"><path d="M1.5 30V3.71875C1.5 2.21721 2.7312 1 4.25 1H20.75C22.2688 1 23.5 2.21721 23.5 3.71875V30L12.5 23.6562L1.5 30Z" stroke="#333333" stroke-width="2"/> </svg>
     `,
-    filter:`<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+    filter: `<svg width="25" height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M23.827 0H1.17324C0.132319 0 -0.392925 1.26299 0.344624 2.00054L9.375 11.0323V21.0938C9.375 21.4761 9.56157 21.8345 9.87485 22.0538L13.7811 24.7872C14.5518 25.3267 15.625 24.7799 15.625 23.8271V11.0323L24.6556 2.00054C25.3916 1.26445 24.87 0 23.827 0Z" fill="#FFF6E9"/>
-    </svg>`
+    </svg>`,
+    car: `<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clip-path="url(#clip0_2395_4074)">
+    <path d="M10.4999 25.6666C11.1443 25.6666 11.6666 25.1443 11.6666 24.5C11.6666 23.8556 11.1443 23.3333 10.4999 23.3333C9.85559 23.3333 9.33325 23.8556 9.33325 24.5C9.33325 25.1443 9.85559 25.6666 10.4999 25.6666Z" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M23.3334 25.6666C23.9777 25.6666 24.5001 25.1443 24.5001 24.5C24.5001 23.8556 23.9777 23.3333 23.3334 23.3333C22.6891 23.3333 22.1667 23.8556 22.1667 24.5C22.1667 25.1443 22.6891 25.6666 23.3334 25.6666Z" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M1.16675 1.16669H5.83341L8.96008 16.7884C9.06677 17.3255 9.35897 17.808 9.78555 18.1514C10.2121 18.4948 10.7459 18.6772 11.2934 18.6667H22.6334C23.1809 18.6772 23.7147 18.4948 24.1413 18.1514C24.5679 17.808 24.8601 17.3255 24.9667 16.7884L26.8334 7.00002H7.00008" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    <defs>
+    <clipPath id="clip0_2395_4074">
+    <rect width="28" height="28" fill="white"/>
+    </clipPath>
+    </defs>
+    </svg>`,
+    member:`<svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M23.3334 24.5V22.1667C23.3334 20.929 22.8417 19.742 21.9666 18.8668C21.0914 17.9917 19.9044 17.5 18.6667 17.5H9.33341C8.09574 17.5 6.90875 17.9917 6.03358 18.8668C5.15841 19.742 4.66675 20.929 4.66675 22.1667V24.5" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M13.9999 12.8333C16.5772 12.8333 18.6666 10.744 18.6666 8.16667C18.6666 5.58934 16.5772 3.5 13.9999 3.5C11.4226 3.5 9.33325 5.58934 9.33325 8.16667C9.33325 10.744 11.4226 12.8333 13.9999 12.8333Z" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    `
 };
