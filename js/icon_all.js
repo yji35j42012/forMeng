4a8363298f6ecf8aec427c0ebe95772e04bb0181
yji35j4212 <yji35j4212@gmail.com>
2022-09-28 17:59:00 +0800

    檔案同步

--- js/icon_all.js
+++ js/icon_all.js
@@ -21,5 +21,12 @@ var icon_all = {
     <path d="M23.3334 24.5V22.1667C23.3334 20.929 22.8417 19.742 21.9666 18.8668C21.0914 17.9917 19.9044 17.5 18.6667 17.5H9.33341C8.09574 17.5 6.90875 17.9917 6.03358 18.8668C5.15841 19.742 4.66675 20.929 4.66675 22.1667V24.5" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M13.9999 12.8333C16.5772 12.8333 18.6666 10.744 18.6666 8.16667C18.6666 5.58934 16.5772 3.5 13.9999 3.5C11.4226 3.5 9.33325 5.58934 9.33325 8.16667C9.33325 10.744 11.4226 12.8333 13.9999 12.8333Z" stroke="#FFF6E9" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
-    `
+    `,
+    close:`<svg width="28" height="29" viewBox="0 0 28 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M21 7.90137L7 21.9014" stroke="#333333" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M7 7.90137L21 21.9014" stroke="#333333" stroke-width="2.91667" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+    
+    `,
+
 };
--- page/car.vue
+++ page/car.vue
@@ -6,12 +6,15 @@
             <li
                 class="step_item boxShadow msg_right on"
                 data-txtr="確認訂單資訊"
-            ></li>
-            <li
-                class="step_item boxShadow msg_right"
-                data-txtr="選擇付款方式"
-            ></li>
-            <li class="step_item boxShadow msg_right" data-txtr="確認結帳"></li>
+            >
+                <span></span>
+            </li>
+            <li class="step_item boxShadow msg_right" data-txtr="選擇付款方式">
+                <span></span>
+            </li>
+            <li class="step_item boxShadow msg_right" data-txtr="確認結帳">
+                <span></span>
+            </li>
         </ul>
         <div class="car_group">
             <div class="car_item boxShadow" v-for="item in 5" :key="item">
@@ -25,10 +28,14 @@
                         <p class="detail_date">10月21日(五) 19:00-21:00</p>
                     </div>
                     <div class="price">
-                        <div>價格</div>
+                        <div class="price_txt">價格</div>
                         <div>$1,200</div>
                     </div>
-                    <button class="normal_btn" data-txt="取消品項"></button>
+                    <div class="cancel">
+                        <button class="normal_btn">取消品項</button>
+                        <i class="icon_close" v-html="icon_all.close"></i>
+                    </div>
+
                     <i class="icon_keep" v-html="icon_all.keep"></i>
                 </div>
             </div>
@@ -36,6 +43,29 @@
         <div class="car_total">
             <button class="normal_btn">訂單總計 $3,600</button>
         </div>
+        <div class="car_subscriber">
+            <div class="car_subscriber_list boxShadow">
+                <div class="title">訂購人</div>
+                <div class="info">
+					<ul class="info_ul">
+						<li class="info_li msg_left" data-txtl="姓名">
+							<input type="text" value="姓名">
+						</li>
+						<li class="info_li msg_left" data-txtl="聯絡電話">
+							<input type="text" value="666777888">
+						</li>
+						<li class="info_li msg_left" data-txtl="Email">
+							<input type="text" value="aaa@gamil.com">
+						</li>
+					</ul>
+				</div>
+            </div>
+            <div class="car_subscriber_list boxShadow">
+                <div class="title">付款方式</div>
+                <div class="info"></div>
+            </div>
+        </div>
+
         <div class="car_func">
             <button class="normal_btn _white">繼續購物</button>
             <button class="normal_btn">確認訂單</button>
