1560255efd9a7c7778051eb565820ddbbea4f92d
yji35j4212 <yji35j4212@gmail.com>
2022-12-02 16:59:13 +0800

    檔案同步

--- page/adm.vue
+++ page/adm.vue
@@ -4,10 +4,17 @@
     <div class="container">
         <div class="admNav">
             <div class="admNav_pic" data-txt="變更頭像">
-                <img src="" alt="">
+                <img src="" alt="" />
             </div>
+            <div class="admNav_share msg_left" data-txtl="已分享食譜 ">0</div>
+            <ul class="admNav_ul boxShadow">
+                <li class="admNav_li on">會員資料</li>
+                <li class="admNav_li">訂單記錄</li>
+                <li class="admNav_li">我的食譜</li>
+                <li class="admNav_li">收藏</li>
+            </ul>
         </div>
-        <div class="admInfo"></div>
+        <router-view></router-view>
     </div>
 </template>
 
