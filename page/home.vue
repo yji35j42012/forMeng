e13daf9e8dbae1ee0c01b89664357d638b4a0624
yji35j4212 <yji35j4212@gmail.com>
2022-12-09 18:19:31 +0800

    檔案同步

--- page/home.vue
+++ page/home.vue
@@ -1,22 +1,21 @@
 <style scoped></style>
 
 <template>
-	<div class="container">
-		home
-	</div>
+    <div class="container">
+        home
+    </div>
 </template>
 
 <script>
 module.exports = {
-	data() {
-		return {};
-	},
-	components: {},
-	mounted() {
-		// console.log(store.state.nowPage);
-	},
-
-	computed: {},
-	methods: {},
+    data() {
+        return {};
+    },
+    components: {},
+    mounted() {
+        // console.log(store.state.nowPage);
+    },
+    computed: {},
+    methods: {},
 };
 </script>
