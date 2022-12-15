9e3abad9482d55226954505bb99860d409273e01
yji35j4212 <yji35j4212@gmail.com>
2022-12-15 17:43:56 +0800

    檔案同步

--- page/home.vue
+++ page/home.vue
@@ -2,7 +2,27 @@
 
 <template>
     <div class="container">
-        home
+        <div class="home_title">
+            「熊齋」源自台語『最知道』諧音，
+            代表我們是最懂素食文化的網站。讓吃素與追求健康生活的你，
+            更容易取得豐富的素食資訊 。
+        </div>
+        <ul class="home_detail">
+            <li class="home_detail_list">
+				<div class="step">
+					<div class="step_num" data-txt="01">01</div>
+					<div class="step_txt">提供平價素食料理課程，實現輕鬆下廚、享樂生活的完美生活提案。</div>
+				</div>
+                <div class="pic">
+                    <img src="../images/home/home_1.png" alt="" />
+                </div>
+            </li>
+            <li class="home_detail_list">
+                <div class="pic">
+                    <img src="../images/home/home_2.png" alt="" />
+                </div>
+            </li>
+        </ul>
     </div>
 </template>
 
